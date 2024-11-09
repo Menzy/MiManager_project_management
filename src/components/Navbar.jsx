9f1dfa8043d08aafc785ed58b7e13092bc538fb8
Menzy <joshuamensah46@gmail.com>
2024-11-09 20:22:36 +0000

    Refactor Navbar component for improved structure and styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,35 +1,45 @@
-import { Link } from 'react-router-dom'
-import { useLogout } from '../hooks/useLogout'
-import { useAuthContext } from '../hooks/useAuthContext'
-
+import { Link } from "react-router-dom";
+import { useLogout } from "../hooks/useLogout";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 // styles & images
-import './Navbar.css'
-import Temple from '../assets/temple.svg'
+import "./Navbar.css";
+import Temple from "../assets/temple.svg";
 
 export default function Navbar() {
-
-  const { logout, isPending } = useLogout()
-  const { user } = useAuthContext()
+  const { logout, isPending } = useLogout();
+  const { user } = useAuthContext();
 
   return (
     <nav className="navbar">
       <ul>
         <li className="logo">
-          <img src={Temple} alt="dojo logo" />
-          <span>MiManage</span>
+          <Link to="/">
+            <div className="logo-content">
+              <img src={Temple} alt="dojo logo" />
+              <span>MiManage</span>
+            </div>
+          </Link>
         </li>
 
-        {!user && <li><Link to="/login">Login</Link></li>}
-        {!user && <li><Link to="/signup">Signup</Link></li>}
+        {!user && (
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        )}
+        {!user && (
+          <li>
+            <Link to="/signup">Signup</Link>
+          </li>
+        )}
         {user && (
           <li>
             <button className="btn" onClick={logout} disabled={isPending}>
-              {isPending ? 'Logging out...' : 'Logout'}
+              {isPending ? "Logging out..." : "Logout"}
             </button>
           </li>
         )}
       </ul>
     </nav>
-  )
+  );
 }
